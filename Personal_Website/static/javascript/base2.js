992dc1c92c9d747e7b6a5174f0f435f5b2186ebc
Arron <arron.thompson1147@gmail.com>
2022-08-02 13:45:45 +1000

    altering base2 js intersection observer to remove the slide up action if the navbar collapases

--- Personal_Website/static/javascript/base2.js
+++ Personal_Website/static/javascript/base2.js
@@ -1,16 +1,22 @@
 const TimeDelay1 = document.querySelectorAll('.link_one');
 observer3 = new IntersectionObserver(entries => {
-  var i = 1000
+  var i = 800
   entries.forEach(entry => {
 
     if(entry.isIntersecting){
 
       const myTimeout = setTimeout(timeDelay1, i);
-
+      const mediaQuery = windo.matchMedia('(min-width: 767px)')
       function timeDelay1() {
-          entry.target.style.visibility="visible"
-          entry.target.classList.add('slideUp');
-          observer3.unobserve(entry.target);
+          if(mediaQuery.matches) {
+            entry.target.classList.remove('slideUp');
+          }
+          else {
+            entry.target.style.visibility="visible"
+            entry.target.classList.add('slideUp');
+            observer3.unobserve(entry.target);
+          }
+
       }
     } else {
       entry.target.classList.remove('slideUp');
